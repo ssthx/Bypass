376ec09dfd02986990f1f319b62ee22d454135af
ssthx <douglinha.k09@gmail.com>
2025-05-20 01:20:25 -0300

    Atualizar o teste.sh

--- teste.sh
+++ teste.sh
@@ -1,16 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Caminho da origem e destino
 ORIGEM="$HOME/Bypass/wall"
 DESTINO="/sdcard/teste/wall"
 
-# Criar pasta destino se não existir
 mkdir -p "/sdcard/teste"
 
-# Verificar e copiar o arquivo
 if [ -f "$ORIGEM" ]; then
     cp "$ORIGEM" "$DESTINO"
-    echo "Arquivo copiado com sucesso para: $DESTINO"
+    echo "Arquivo copiado para: $DESTINO"
 else
     echo "Arquivo de origem não encontrado: $ORIGEM"
 fi
